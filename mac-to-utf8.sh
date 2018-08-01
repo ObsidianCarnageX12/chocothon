9ea65631c0f81e1b48307199e562d9068a821603
Brandon Clark <marathonspeedruns@gmail.com>
2018-07-31 21:40:06 -0400

    Removed mac-to-utf8.sh script.

--- mac-to-utf8.sh
+++
@@ -1,12 +0,0 @@
-#!/bin/bash
-
-# Takes any number of input files with CR line endings in Macintosh Roman and
-# converts them to Unix line endings and UTF-8. Used to convert the original
-# Marathon Infinity source files to be compatible with modern editors.
-
-while [ "${1}" != "" ]; do
-	echo "Converting ${1}..."
-	iconv -f mac -t utf8 "${1}" | sed 's//\n/g' > ${1}
-  shift # move to the next argument
-done
-
